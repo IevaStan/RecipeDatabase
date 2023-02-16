c840d65fbaf72c13e081def7ac47bde0c7f7c363
IevaStan <101921345+IevaStan@users.noreply.github.com>
2023-02-16 09:09:44 +0200

    connections between tables made

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Recipe extends Model
 {
@@ -15,4 +17,13 @@ class Recipe extends Model
         'description',
         'is_active'
     ];
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
